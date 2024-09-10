d948c381d3edae334d27f362127295fb69cdcb84
bschulte1515 <112735843+bschulte1515@users.noreply.github.com>
2024-09-10 16:13:46 -0500

    Create ships.cpp
    Implementation for Ship class and methods.

---
+++ src/ships.cpp
@@ -0,0 +1,75 @@
+#include <vector>
+#include <utility>
+#include "ships.hpp"
+
+  // Initialize ship based on size
+Ship::Ship(int size) : shipSize(size) {
+  spaces.resize(size); // Give the ship enough space for its size
+}
+
+// Place the ship on the board
+bool Ship::place(std::vector<std::pair<int, int>>& coords) {
+  if (coords.size() != shipSize) {
+    return false;
+  }
+  for (int i = 0; i < shipSize; i++) {
+    spaces[i] = {coords[i], false};
+  }
+  return true;
+}
+
+// Remove the ship from the board if we need to during setup
+void Ship::remove() {
+  spaces.clear();
+}
+
+ // Hit a space occupied by the ship
+void Ship::hit(std::pair<int, int>& coord) {
+  for (auto& space : spaces) {
+    if (coord == space.first) {
+      space.second = true;
+      return;
+    }
+  }
+}
+
+// Check a coordinate for being controlled by this ship
+bool Ship::valid_space(std::pair<int, int>& coord) {
+  for (auto& space : spaces) {
+    if (coord == space.first) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Check a coordinate for being hit
+bool Ship::is_hit(std::pair<int, int>& coord) {
+  for (auto& space : spaces) {
+    if (coord == space.first)
+      return space.second;
+  }
+  return false;
+}
+
+// Check if the ship is sunk
+bool Ship::is_sunk() { 
+  for (auto& space : spaces) {
+    if (space.second == false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Returns this ship's spaces and hit status's
+std::vector<std::pair<std::pair<int, int>, bool>>& Ship::get_spaces() {
+  return spaces;
+}
+
+
+OneShip::OneShip() : Ship(1) {}  // Call the base class constructor with size 1
+TwoShip::TwoShip() : Ship(2) {}  // Call the base class constructor with size 2
+ThreeShip::ThreeShip() : Ship(3) {}  // Call the base class constructor with size 3
+FourShip::FourShip() : Ship(4) {}  // Call the base class constructor with size 4
+FiveShip::FiveShip() : Ship(5) {}  // Call the base class constructor with size 5

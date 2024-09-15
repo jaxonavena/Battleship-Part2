932c700c21a24a4cae9fccc3104ba3914702cc15
jDeBok <113641695+jDeBok@users.noreply.github.com>
2024-09-15 14:34:56 -0500

    Added comments

--- src/player.hpp
+++ src/player.hpp
@@ -1,3 +1,8 @@
+/*
+Program: Player header file
+Description: Contains methods implemented within player
+Author: Team 9
+*/
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 using namespace std;
@@ -6,6 +11,7 @@ using namespace std;
 #include <memory>
 #include <string>
 #include "ships.hpp"
+#include "board.hpp"
 
 class Player {
 
@@ -14,12 +20,13 @@ class Player {
         string returnName() const;
         shared_ptr<Ship> getShip( const size_t shipSize );
         void setupShips();
-
+        void print_Board(); //prints both top and bottom board
     private:
         string name; //name of the player
         array<shared_ptr<Ship>, 5> shipArray; //A std::array of shared pointers whose size is 5
         int numofShips;
-
+        Board* top_board;    //top board, shows this player's shots and hits
+        Board* bottom_board; //bottom board, shows this player's ships
 };
 
 #endif

a11cd43c978eb424f3d413d4c6016f78839c9d67
jDeBok <113641695+jDeBok@users.noreply.github.com>
2024-09-15 22:53:33 -0500

    Added comments

--- src/gameloop.cpp
+++ src/gameloop.cpp
@@ -15,24 +15,24 @@ Author: Team 9
 #include <utility>
 
 //Initialize gameplay loop by taking rvalue refs 
-GameplayLoop::GameplayLoop(Player&& p1, Player&& p2) : 
-                           playerOne(std::move(p1)), playerTwo(std::move(p2)),
+GameplayLoop::GameplayLoop(Player&& p1, Player&& p2) :  //use references to prevent memory issues
+                           playerOne(std::move(p1)), playerTwo(std::move(p2)), //create players within this class.
                            currentTurn(1){} //this moves the player objects 
 
 //Simple function that returns true if a shot is valid to take
 bool GameplayLoop::verifyShot(const std::size_t row, const std::size_t col) const {
     //If the cell of the top board is X or O that means the shot has already been taken
     if ( !currentTurn ) {
-        if ( playerOne.getCellTopBoard(row, col) == 'X' || playerOne.getCellTopBoard(row, col) == 'O' ) {
-            return false;
+        if ( playerOne.getCellTopBoard(row, col) == 'X' || playerOne.getCellTopBoard(row, col) == 'O' ) { //check if shot already taken
+            return false; //verify shot as bad, don't continue
         }
     } else {
-        if ( playerTwo.getCellTopBoard(row, col) == 'X' || playerTwo.getCellTopBoard(row, col) == 'O' ) {
-            return false;
+        if ( playerTwo.getCellTopBoard(row, col) == 'X' || playerTwo.getCellTopBoard(row, col) == 'O' ) { //check if already takena
+            return false; //verify shot as bad, don't continue
         }
     }
 
-    return true;
+    return true; //otherwise shot is good, continue
 }
 
 //This function gets a shot from the user in the format of RowCol
@@ -69,7 +69,7 @@ std::pair<std::size_t, std::size_t> GameplayLoop::getShot() const {
         if ( !verifyShot(shot_row, playerOne.convert_chartoIndex( column ))) {
             std::cout << "You have already taken this shot! Try again.";
         } else {
-            return std::make_pair(shot_row, playerOne.convert_chartoIndex( column ));
+            return std::make_pair(shot_row, playerOne.convert_chartoIndex( column )); //create pair to return for shot validation
         }
     }
 
@@ -77,90 +77,90 @@ std::pair<std::size_t, std::size_t> GameplayLoop::getShot() const {
 
 void GameplayLoop::playerOneTurn() {
     //Player 1 takes their turn
-    std::cout << "Player 1's Turn." << std::endl;
+    std::cout << "Player 1's Turn." << std::endl; //output player turn
     playerOne.print_Board(); //print player 1's board
     std::pair<std::size_t, std::size_t> coord = getShot();//pair that gets the shot from the user
 
-    int flag = 0;
+    int flag = 0; //output ship
 
     for (int i = 1; i < playerOne.getNumShips() + 1; i++) {
         if (playerTwo.getShip(i)->valid_space(coord)) { //if is_hit, update board and ship
-            flag = i;
-            break;
+            flag = i; //figure out which ship has been marked
+            break; //exit loop
         }
     }
 
     if (flag > 0) {
         playerOne.top_board.update(coord, true); //update board
         playerTwo.getShip(flag)->hit(coord); //hit the ship
-        playerTwo.bottom_board.update(coord, true);
+        playerTwo.bottom_board.update(coord, true); //update bottom board
     }
     else {
-        playerOne.top_board.update(coord, false);
-        playerTwo.bottom_board.update(coord, false);
+        playerOne.top_board.update(coord, false); //update with miss
+        playerTwo.bottom_board.update(coord, false); //update with miss
     }
 
     playerOne.print_Board(); //reprint board(s)
-    sleep(4);
+    sleep(4); //sleep for hot seat, wait for next player
     system("clear"); //clear the terminal before the next action
 }
 
 void GameplayLoop::playerTwoTurn() {
     //Player 2 takes their turn
-    std::cout << "Player 2's Turn." << std::endl;
+    std::cout << "Player 2's Turn." << std::endl; //output player's board
     playerTwo.print_Board(); //print player 2's board
     std::pair<std::size_t, std::size_t> coord = getShot(); //pair that gets the shot from the user
 
-    int flag = 0;
+    int flag = 0; //output ship
 
     for (int i = 1; i < playerOne.getNumShips() + 1; i++) {
         if (playerOne.getShip(i)->valid_space(coord)) { //if is_hit, update board and ship
-            flag = i;
-            break;
+            flag = i; //which ship has been hit
+            break; //exit loop
         }
     }
 
-    if (flag > 0) {
+    if (flag > 0) { //ship has been hit
         playerTwo.top_board.update(coord, true); //update board
         playerOne.getShip(flag)->hit(coord); //hit the ship
-        playerOne.bottom_board.update(coord, true);
+        playerOne.bottom_board.update(coord, true); //update bottom board
     }
     else {
-        playerTwo.top_board.update(coord, false);
-        playerOne.bottom_board.update(coord, false);
+        playerTwo.top_board.update(coord, false); //update top board no hit
+        playerOne.bottom_board.update(coord, false); //update bottom board no hit
     }
 
     playerTwo.print_Board(); //reprint board(s)
-    sleep(4);
+    sleep(4); //sleep for hot seat.
     system("clear"); //clear the terminal before the next action
 }
 
 bool GameplayLoop::gameOver() const {
-    // Iterate through both players' ships and return true iff all ships are sunk for one player
-    const int numShips = playerOne.getNumShips();
-    bool playerOneSunk = true;
-    bool playerTwoSunk = true;
-    for (int i = 1; i < numShips + 1; i++) {
-        if (!(playerOne.getShip(i)->is_sunk())) {
-            playerOneSunk = false;
+    // Iterate through both players' ships and return true iff (if and onl if) all ships are sunk for one player
+    const int numShips = playerOne.getNumShips(); //num ships to check for
+    bool playerOneSunk = true; //set flag
+    bool playerTwoSunk = true; //set flag
+    for (int i = 1; i < numShips + 1; i++) { //check over all ships
+        if (!(playerOne.getShip(i)->is_sunk())) { //check all ships are sunk
+            playerOneSunk = false; //one ship is not sunk, game NOT over
         }
     }
-    for (int i = 1; i < numShips + 1; i++) {
-        if (!(playerTwo.getShip(i)->is_sunk())) {
-            playerTwoSunk = false;
+    for (int i = 1; i < numShips + 1; i++) { //check player two's ships
+        if (!(playerTwo.getShip(i)->is_sunk())) { //check all ships owned by player 2
+            playerTwoSunk = false; //one ship not sunk, game NOT over
         }
     }
-    if (playerOneSunk) {
-        std::cout << playerOne.returnName() << "'s ships have been sunk. "
-                  << playerTwo.returnName() << " wins!" << std::endl;
-        return true;
+    if (playerOneSunk) { //check if player one loses
+        std::cout << playerOne.returnName() << "'s ships have been sunk. " //use name to show who loses
+                  << playerTwo.returnName() << " wins!" << std::endl; //show name to see who wins
+        return true;  //for game play loop class
     }
-    else if (playerTwoSunk) {
-        std::cout << playerTwo.returnName() << "'s ships have been sunk. "
-                  << playerOne.returnName() << " wins!" << std::endl;
-        return true;
+    else if (playerTwoSunk) { //check if player two loses
+        std::cout << playerTwo.returnName() << "'s ships have been sunk. " //use player two name to lose
+                  << playerOne.returnName() << " wins!" << std::endl; //use player one name to lose
+        return true; //there is a loss here
     }
-    return false;
+    return false; //no loss
 }
 
 void GameplayLoop::start() {
@@ -173,7 +173,7 @@ void GameplayLoop::start() {
         ++currentTurn %= 2;
         //if currentTurn == 0
         if ( !currentTurn ) {
-            playerOneTurn();
+            playerOneTurn(); //get the correct player's turn
         } else {
             playerTwoTurn();
         }

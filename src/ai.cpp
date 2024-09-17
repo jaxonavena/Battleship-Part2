0d7205a7ab7de7c66ab417d8bec6fba2f100208e
Jaxon Avena <jaxonavena@gmail.com>
2024-09-17 18:15:51 -0500

    Init AI opponent.

--- src/ai.cpp
+++ src/ai.cpp
@@ -1,1 +1,14 @@
-// Comment!
+#include "ai.hpp"
+
+// constructor definition
+AI::AI(int difficulty) : difficulty(difficulty) {}
+
+// getters
+int AI::getDifficulty() const {
+  return difficulty;
+}
+
+// setters
+void AI::setDifficulty(int newDifficulty) {
+  difficulty = newDifficulty;
+}
--- src/ai.hpp
+++ src/ai.hpp
@@ -1,1 +1,21 @@
-// Comment!
+#ifndef AI_HPP
+#define AI_HPP
+
+#include <string>
+
+class AI {
+public:
+  // constructor
+  AI(int difficulty);
+
+  // getters
+  int getDifficulty() const;
+
+  // setters
+  void setDifficulty(int difficulty);
+
+private:
+  int difficulty;
+};
+
+#endif
